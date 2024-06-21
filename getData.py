0fca6238f53faf6e2195fc0632b306db2c753ba7
Jake <essigj88@outlook.com>
2024-06-20 23:35:15 -0400

    prototype get api

--- getData.py
+++ getData.py
@@ -1,1 +1,8 @@
- 
+import requests
+import json
+
+def getDate():
+    t_rates = "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/TextView?type=daily_treasury_real_long_term&field_tdr_date_value=2005"
+    response = requests.get(t_rates)
+    print(response.json())
+getDate()
